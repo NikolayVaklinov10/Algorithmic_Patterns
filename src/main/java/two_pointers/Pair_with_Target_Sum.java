d0dda4e736f56fc138ce4684569664f42c14f7a8
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-10-26 21:39:07 +0100

    searching for a particular target sum in sorted array

---
+++ src/main/java/two_pointers/Pair_with_Target_Sum.java
@@ -0,0 +1,25 @@
+package two_pointers;
+
+public class Pair_with_Target_Sum {
+
+
+        public static int[] search(int[] arr, int targetSum){
+
+            int left = 0, right=arr.length-1;
+            while (left < right){
+                int currentSum = arr[left] + arr[right];
+                if(currentSum == targetSum){
+                    return new int[] {left, right}; // found the pair
+                } else if (targetSum > currentSum){
+                    left++; // we need a pair with bigger sum
+                } else {
+                    right--;
+                }
+            }
+            return new int[]{-1, -1};
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
