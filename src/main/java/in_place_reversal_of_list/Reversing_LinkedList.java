fd51a241d697d5a89efe05dd99708d97f684f356
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-10-29 22:28:46 +0100

    reversing a linked list

---
+++ src/main/java/in_place_reversal_of_list/Reversing_LinkedList.java
@@ -0,0 +1,32 @@
+package in_place_reversal_of_list;
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
+
+public class Reversing_LinkedList {
+
+    public static ListNode reverse(ListNode head) {
+        ListNode current = head; // current node that we will be processing
+        ListNode previous = null; // previous node that we have processed
+        ListNode next = null; // will be used to temporarily store the next node
+
+        while (current != null) {
+            next = current.next; // temporarily store the next node
+            current.next = previous; // reverse the current node
+            previous = current; // before we move to the next node, point previous to the current node
+            current = next; // move on the next node
+        }
+        // after the loop current will be pointing to 'null' and 'previous' will be the new head
+        return previous;
+    }
+
+
+
+
+}
