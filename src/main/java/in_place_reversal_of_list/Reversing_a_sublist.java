65c2c8b3b2841fa19590f2f485b3561adbf654d6
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-11-01 22:26:41 +0000

    testing the code

--- src/main/java/in_place_reversal_of_list/Reversing_a_sublist.java
+++ src/main/java/in_place_reversal_of_list/Reversing_a_sublist.java
@@ -48,5 +48,20 @@ public class Reversing_a_sublist {
         return head;
     }
 
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode result = Reversing_a_sublist.reverse(head, 2, 4);
+        System.out.print("Nodes of the reversed LinkedList are: ");
+        while (result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+    }
+
 
 }
