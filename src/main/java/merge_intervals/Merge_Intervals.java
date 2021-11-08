e6f8d6cebb47f7f2936506aad4adfe824931270e
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-11-08 23:24:39 +0000

    the way to merge 2 intervals

--- src/main/java/merge_intervals/Merge_Intervals.java
+++ src/main/java/merge_intervals/Merge_Intervals.java
@@ -1,8 +1,6 @@
 package merge_intervals;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 class Interval {
     int start;
@@ -17,8 +15,31 @@ class Interval {
 public class Merge_Intervals {
 
     public static List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size() < 2)
+            return intervals;
+
+        // sort the intervals by start time
+        Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+
         List<Interval> mergedIntervals = new LinkedList<Interval>();
-        // TODO: Write your code here
+        Iterator<Interval> intervalItr = intervals.iterator();
+        Interval interval = intervalItr.next();
+        int start = interval.start;
+        int end = interval.end;
+
+        while (intervalItr.hasNext()) {
+            interval = intervalItr.next();
+            if (interval.start <= end) { // overlapping intervals, adjust the 'end'
+                end = Math.max(interval.end, end);
+            } else { // non-overlapping interval, add the previous interval and reset
+                mergedIntervals.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+        // add the last interval
+        mergedIntervals.add(new Interval(start, end));
+
         return mergedIntervals;
     }
 
